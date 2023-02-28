81aea94275d69fc3e9e59d7909d298bc89fb0636
Vincius Vequeti <viniciusvequeti@gmail.com>
2023-02-28 08:27:32 -0300

    departmentDao methods implemented

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,7 +11,7 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 
 		DepartmentDao departmentDao = DaoFactory.createDepartmentrDao();
 		System.out.println("=== TEST 1: department finById ===");
@@ -24,6 +25,23 @@ public class Program2 {
 			System.out.println(dep);
 		}
 
+		System.out.println("\n=== TEST 3: department insert ===");
+		Department newDep = new Department(null, null);
+		departmentDao.insert(newDep);
+		System.out.println("Inserted! New id = " + newDep.getId());
+		
+		System.out.println("\n=== TEST 4: department update ===");
+		department = departmentDao.findById(2);
+		department.setName("Music");
+		departmentDao.update(department);
+		System.out.println("Update completed!");
+		
+		System.out.println("\n=== TEST 4: department deleteById ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
-
 }
